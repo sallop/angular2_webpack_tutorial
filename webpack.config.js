298e2a70fc777f9cdf463c9e54f9fba7a5306d44
sallop <shiogai1987@yahoo.co.jp>
2016-03-03 17:34:51 +0900

    add configuration for angular2

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,29 @@
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
-  entry: './src/app.ts',
+  entry: {
+    bundle: ['webpack/hot/dev-server','./src/main.ts'],
+    //bundle: ['webpack/hot/dev-server','./src/test.js'],
+    vendor: ['zone.js', 'reflect-metadata', 'angular2/platform/browser'],
+  },
   output: {
     path: './build',
-    filename: 'bundle.js'
+    filename: '[name].js'
   },
   resoleve: {
     extensions: ['', '.webpack.js', '.web.js', '.ts', '.tsx', '.js']
   },
+  plugins: [
+    //new HtmlWebpackPlugin({
+    //  title: 'My App',
+    //  filename: './index.html'
+    //}),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.optimize.CommonsChunkPlugin(/* chunkName */'vendor', /* fileName */'vendor.bundle.js')
+  ],
+  //devtool: 'source-map',
+  devtool: 'eval',
   module: {
     loaders: [
       { test: /\.tsx?$/, loader: 'ts-loader' }
